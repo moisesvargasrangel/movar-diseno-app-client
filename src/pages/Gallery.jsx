8c2bdb2423dfce88afed2d3131e4a78932360c6c
Moisés Vargas Rangel <vargasrangelaudio@gmail>
2022-07-31 19:41:54 -0500

    LogIn/SignUp - Form - OK // Update Views - My Products

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,59 +1,82 @@
+/* This example requires Tailwind CSS v2.0+ */
 const products = [
   {
     id: 1,
-    name: 'Earthen Bottle',
+    name: 'Cocina Integral',
+    color: 'Muebles en MDF, con cubierta de Piedra',
     href: '#',
-    price: '$48',
     imageSrc: '/images/gallery/1.jpg',
-    imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+    imageAlt: 'Mueble 1',
+    price: '$20,000.00',
   },
   {
-    id: 2,
-    name: 'Nomad Tumbler',
+    id: 1,
+    name: 'Vestidor',
+    color: 'Muebles en MDF color Rioja',
     href: '#',
-    price: '$35',
     imageSrc: '/images/gallery/2.jpg',
-    imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+    imageAlt: 'Mueble 1',
+    price: '$14,000.00',
   },
   {
-    id: 3,
-    name: 'Focus Paper Refill',
+    id: 1,
+    name: 'Cocina Integral',
+    color: 'Muebles en MDF, con cubierta de Piedra',
     href: '#',
-    price: '$89',
     imageSrc: '/images/gallery/3.jpg',
-    imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    imageAlt: 'Mueble 1',
+    price: '$18,000.00',
   },
   {
-    id: 4,
-    name: 'Machined Mechanical Pencil',
+    id: 1,
+    name: 'Cocina Integral',
+    color: 'Muebles en MDF, con cubierta de Piedra',
     href: '#',
-    price: '$35',
     imageSrc: '/images/gallery/4.jpeg',
-    imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    imageAlt: 'Mueble 1',
+    price: '$13,000.00',
   },
   // More products...
 ]
 
 export default function Gallery() {
-  
   return (
     <div className="bg-white">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
+        <h2 className="text-xl font-bold text-gray-900">Nuestros Productos</h2>
 
-        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
-              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="w-full h-full object-center object-cover group-hover:opacity-75"
-                />
+            <div key={product.id}>
+              <div className="relative">
+                <div className="relative w-full h-72 rounded-lg overflow-hidden">
+                  <img
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    className="w-full h-full object-center object-cover"
+                  />
+                </div>
+                <div className="relative mt-4">
+                  <h3 className="text-sm font-medium text-gray-900">{product.name}</h3>
+                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                </div>
+                <div className="absolute top-0 inset-x-0 h-72 rounded-lg p-4 flex items-end justify-end overflow-hidden">
+                  <div
+                    aria-hidden="true"
+                    className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
+                  />
+                  <p className="relative text-lg font-semibold text-white">{product.price}</p>
+                </div>
               </div>
-              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
-            </a>
+              <div className="mt-6">
+                <a
+                  href={product.href}
+                  className="relative flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200"
+                >
+                  Agregar al carrito <span className="sr-only">, {product.name}</span>
+                </a>
+              </div>
+            </div>
           ))}
         </div>
       </div>
